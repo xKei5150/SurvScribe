aea93d7c0aa48b500758960e6bd18d383fb8ded3
Von Maverick <vmaverick22@gmail.com>
2024-02-03 13:33:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/survscribe/st_test_layout.java
@@ -0,0 +1,12 @@
+package com.example.survscribe;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class st_test_layout extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_st_test_layout);
+    }
+}
